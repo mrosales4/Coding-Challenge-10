a7317bbc1350b9431674f3d7e46177dc40be33d8
Matthew A Rosales <matthewrosales@usf.edu>
2024-06-18 20:24:51 -0400

    Implement data loading and DOM manipulation with D3.js.

---
+++ example.js
@@ -0,0 +1,15 @@
+//U22978120
+
+// function to load data from a CSV file
+d3.csv("data/purchase_orders.csv")
+  .then(data => {
+    const ul = d3.select("#purchase-orders");
+
+    data.forEach(order => {
+      const li = ul.append("li") //append element
+        .text(`${order.customerName} - Order ID: ${order.orderId} - Purchase Amount: ${order.purchaseAmount}`);
+    });
+  })
+  .catch(error => {
+    console.error("Error loading data:", error);
+  });
